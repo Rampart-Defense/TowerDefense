878889a4d924f62b140464f3e9cf99909a44325c
Valheri <valtteri.vuokila@gmail.com>
2025-10-21 11:52:55 +0300

    Enemy double death fix, mainmenu music volume increase

--- Scripts/Menus/menumusic.gd
+++ Scripts/Menus/menumusic.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	volume_db = -40 # start very quiet
 	play()
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "volume_db", -20, 3.0) # fade in over 2 seconds
+	tween.tween_property(self, "volume_db", 0, 3.0) # fade in over 2 seconds
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
