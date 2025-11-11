74a99d6928849de8005f4f586342ff18b6075615
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-11-11 19:46:55 +0200

    Added Start waves -button on the sidepanel

--- Scripts/UI/side_panel.gd
+++ Scripts/UI/side_panel.gd
@@ -1,20 +1,27 @@
 extends Control
 @onready var towershop= $TowerShop
-
+@onready var startWaveButton = $TextureButton
 @onready var panel = $Panel
 var is_hidden = false
+
 func hide_shop():
 	towershop.hide()
 
 func show_shop():
 	towershop.show()
-	
 
 	
 func hide_all():
 	panel.hide()
 	towershop.hide()
+	startWaveButton.hide()
+	
 
 func show_all():
 	panel.show()
 	towershop.show()
+	startWaveButton.show()
+
+
+func _on_start_wave_button_down() -> void:
+	Waves.begin()
